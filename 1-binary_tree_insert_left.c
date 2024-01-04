0a9e08a47094af648c3484192ae6549ce85f0b40
isaquliyev <isa.quluyev.wip@gmail.com>
2024-01-04 15:10:52 -0800

    Unused variable temp removed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,7 +11,6 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-	binary_tree_t *temp = parent;
 
 	new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node || !parent)
