367bd39c2613769281d6548257c84f10412b094f
isaquliyev <isa.quluyev.wip@gmail.com>
2024-01-04 14:36:24 -0800

    If malloc fails, then free the new_node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -16,7 +16,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node)
+	{
+		free(new_node);
 		return (NULL);
+	}
 	new_node->n = value;
 	if (!parent)
 		parent = new_node;
