05115130bf52499edefae64c1927f3fab92d2230
isaquliyev <isa.quluyev.wip@gmail.com>
2024-01-05 06:39:48 -0800

    binary_tree_uncle function implemented.

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - Finds the uncle of a node.
+ *
+ * @node: Pointer to the node to find the uncle.
+ *
+ * Return: Pointer to the uncle node.
+ * If node is NULL, return NULL.
+ * If node has no uncle, return NULL
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent || !node->parent->parent)
+		return (NULL);
+	if (node->parent == node->parent->parent->left)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
+}
