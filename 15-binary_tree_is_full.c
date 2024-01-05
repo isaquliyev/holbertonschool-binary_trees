4e1f0e51c9609fe709238e361f5800b14e612bf5
isaquliyev <isa.quluyev.wip@gmail.com>
2024-01-05 05:17:09 -0800

    2 * leaves - 1 == size, then tree is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,27 +13,26 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	if (2 * binary_tree_height(tree) + 1 == binary_tree_size(tree))
+	if (2 * binary_tree_leaves(tree) - 1 == binary_tree_size(tree))
 		return (1);
 	return (0);
 }
 
 /**
- * binary_tree_height - Measures the height of a binary tree.
+ * binary_tree_leaves - Counts the leaves in a binary tree.
  *
  * @tree: Pointer to the root node of the tree.
  *
- * Return: Height of the tree. If tree is NULL return 0.
+ * Return: Number of leaves, if tree is NULL returns 0.
  */
 
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	if (!tree || (!tree->left && !tree->right))
+	if (!tree)
 		return (0);
-	if (binary_tree_height(tree->left) > binary_tree_height(tree->right))
-		return (1 + binary_tree_height(tree->left));
-	else
-		return (1 + binary_tree_height(tree->right));
+	if (!tree->left && !tree->right)
+		return (1);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
 
 /**
