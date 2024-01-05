dea22d27996da169544bfc527e2d86acaa75a635
isaquliyev <isa.quluyev.wip@gmail.com>
2024-01-05 05:06:34 -0800

    2 * height + 1 == size, then tree is full.

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,7 +11,42 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree && tree->left && tree->right && tree->parent)
+	if (!tree)
+		return (0);
+	if (2 * binary_tree_height(tree) + 1 == binary_tree_size(tree))
 		return (1);
 	return (0);
+}
+
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ *
+ * @tree: Pointer to the root node of the tree.
+ *
+ * Return: Height of the tree. If tree is NULL return 0.
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+        if (!tree || (!tree->left && !tree->right))
+                return (0);
+        if (binary_tree_height(tree->left) > binary_tree_height(tree->right))
+                return (1 + binary_tree_height(tree->left));
+        else
+                return (1 + binary_tree_height(tree->right));
+}
+
+/**
+ * binary_tree_size - Measures the size of a binary tree.
+ *
+ * @tree: Pointer to the root node of the tree.
+ *
+ * Return: Size of tree, if tree NULL returns 0.
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+        if (!tree)
+                return (0);
+        return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
